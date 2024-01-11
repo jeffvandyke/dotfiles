a039099f107eabe3f591248ce2addbdd47c9856d
Jeff VanDyke <jeffrey.vandyke@instacart.com>
2024-01-11 14:24:08 -0500

    Add formatter

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,7 @@ opt.cursorline = true
 opt.cursorlineopt = "both"
 opt.scrolloff = 2
 opt.termguicolors = true
+opt.background = "light"
 
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -21,40 +22,40 @@ opt.linebreak = true
 opt.showbreak = "▶▶━┫"
 opt.inccommand = "nosplit"
 
-vim.keymap.set('n', 'ZW', ':w<CR>')
+vim.keymap.set("n", "ZW", ":w<CR>")
 
 opt.swapfile = false
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-    pattern = { "*" },
-    command = "wincmd =",
+	pattern = { "*" },
+	command = "wincmd =",
 })
 
-vim.keymap.set('n', 'gx', [[:silent! execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]])
+vim.keymap.set("n", "gx", [[:silent! execute '!open ' . shellescape(expand('<cfile>'), 1)<CR>]])
 
 -- Instantly save and load
 opt.autoread = true
 opt.autowriteall = true
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-    pattern = { "*" },
-    command = ":silent! !",
+	pattern = { "*" },
+	command = ":silent! !",
 })
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
-    pattern = { "*" },
-    command = ":silent! update",
+	pattern = { "*" },
+	command = ":silent! update",
 })
 
 -- Lazy.nvim bootstrap - see https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -64,9 +65,9 @@ vim.opt.rtp:prepend(lazypath)
 -- vim.g.loaded_netrwPlugin = 1
 
 if not vim.g.lazy_did_setup then
-    require("lazy").setup("plugins", {
-        change_detection = { notify = false },
-    })
+	require("lazy").setup("plugins", {
+		change_detection = { notify = false },
+	})
 end
 --[[
 ./lua/plugins.lua ]]
